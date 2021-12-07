8b67c2dd6ff578e2dd704819d19b1c8455304465
Cara Noyes <cnnnoyes@outlook.com>
2021-12-07 00:09:40 -0600

    new-user-form files created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NewUserFormComponent } from './new-user-form/new-user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-user-form/new-user-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user-form',
+  templateUrl: './new-user-form.component.html',
+  styleUrls: ['./new-user-form.component.css']
+})
+export class NewUserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
