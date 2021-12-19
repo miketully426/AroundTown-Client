328c3098d7bf1867471742c0fac1f8724e35a837
Eydie <you@example.com>
2021-12-19 16:25:37 -0600

    all features working but need to fix error msg before pull request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { UserFormComponent } from './user-form/user-form.component';
-import { EventListComponent } from './event-list/event-list.component'
+import { EventListComponent } from './event-list/event-list.component';
 import { EventFormComponent } from './event-form/event-form.component';
+import { SearchResultsByIdComponent } from './search-results-by-id/search-results-by-id.component';
 
 const routes: Routes = [
   // {path: 'list-user', redirectTo: '/', pathMatch: 'full'},
   { path: 'events', component: EventListComponent },
   {path: 'addevent', component: EventFormComponent},
-  {path: 'adduser', component: UserFormComponent}  
+  {path: 'adduser', component: UserFormComponent},
+  {path: 'searchresultsbyid', component: SearchResultsByIdComponent},  
 ];
 
 @NgModule({
