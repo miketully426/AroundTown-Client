e40afb374a51d189a5400396e838e7c059e4419e
Eydie <you@example.com>
2021-12-27 20:40:18 -0600

    changed some stuff in component html

--- src/app/search-results-by-id/search-results-by-id.component.ts
+++ src/app/search-results-by-id/search-results-by-id.component.ts
@@ -28,7 +28,7 @@
           let matchingEventIds: Event[] = [];
           for(let i=0; i < this.events.length; i++) {
             for(let property in this.events[i]) {
-                if(this.events[i][property].toString() === searchId.toString()) {
+                if(this.events[i][property].toString() === searchId) {
                   matchingEventIds.push(this.events[i]);
                   break;
                   
