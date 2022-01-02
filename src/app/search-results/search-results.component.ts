a397defca1cc2a9f7b05fea72224fb4ba2ddc303
Jessica Mertz <mertzjl91@gmail.com>
2022-01-01 22:35:26 -0600

    all filters working, error message displays. The only thing is an error if a semicolon is included...

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -10,6 +10,9 @@ import { EventService } from '../service/event-service.service';
 export class SearchResultsComponent implements OnInit {
 
   searchedEvents: Event[] = [];
+  famFriendly: boolean;
+  lowPrice: number;
+  highPrice: number;
 
   constructor(private eventService: EventService) { }
 
@@ -17,15 +20,65 @@ export class SearchResultsComponent implements OnInit {
 
   }
 
-  searchByKeyword(searchTerm: string, filter: string) {
-    // if(!searchTerm) { //change this to handle no search term WITH a filter
-    //   this.eventService.findAll().subscribe(searchData => {this.searchedEvents = searchData});
-    // } else {
-      this.eventService.viewMatchingEventsByKeyword(searchTerm.toLowerCase(), filter).subscribe(searchData => {
-        this.searchedEvents = searchData;
-      });
+  onSearch(searchTerm: string, familyFriendlyFilter: String, priceFilter: string) {
+    if(familyFriendlyFilter == "familyFriendly") {
+      this.famFriendly = true;
+    } else {
+      this.famFriendly = false;
     }
-  // }
+     
+    if(priceFilter == "free") {
+      this.lowPrice = 0;
+    } else if(priceFilter == "$") {
+      this.lowPrice = 1;
+      this.highPrice = 26;
+    } else if(priceFilter == "$$") {
+      this.lowPrice = 26;
+      this.highPrice = 51;
+    } else if(priceFilter == "$$$") {
+      this.lowPrice = 51;
+      this.highPrice = 76;
+    } else if(priceFilter == "$$$$") {
+      this.lowPrice = 76;
+      this.highPrice = 101;
+    }else if(priceFilter == "$$$$$") {
+      this.lowPrice = 100;
+    }   
+
+  
+    if(!searchTerm) {
+        if(priceFilter == "none" && familyFriendlyFilter == "none") {
+          this.eventService.findAll().subscribe(allEvents => 
+            {this.searchedEvents = allEvents});
+        } else if (priceFilter == "none") {
+          this.eventService.viewAllFamFriendly(this.famFriendly).subscribe(famFriendly => 
+            {this.searchedEvents = famFriendly});
+        } else if(familyFriendlyFilter == "none") {
+            this.eventService.viewAllByPrice(this.lowPrice, this.highPrice).subscribe(allEventCost => {this.searchedEvents = allEventCost});  
+        } else {
+          this.eventService.viewAllByFamFriendlyAndPrice(this.famFriendly, this.lowPrice, this.highPrice).subscribe(famFriendCost => 
+            {this.searchedEvents = famFriendCost});
+        }
+    }
+
+    if(searchTerm) {
+      if(priceFilter == "none" && familyFriendlyFilter == "none") {
+        this.eventService.searchByKeywordNoFilter(searchTerm).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else if(priceFilter == "none") {
+        this.eventService.searchByKeywordFamFriendly(searchTerm, this.famFriendly).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else if(familyFriendlyFilter == "none") {
+        this.eventService.searchByKeywordPrice(searchTerm, this.lowPrice, this.highPrice).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else {
+        this.eventService.searchByKeywordFamFriendlyPrice(searchTerm, this.famFriendly, this.lowPrice, this.highPrice).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      }
+    }  
+    this.highPrice = null;
+
+  }
 
 }
 
--- src/app/service/event-service.service.ts
+++ src/app/service/event-service.service.ts
@@ -22,16 +22,48 @@ export class EventService {
      return this.http.post<Event>(this.eventsURL, event);
    }
 
-   public searchEventsByKeyword(searchTerm: String) {
-     return this.http.post<String>(`${this.eventsURL}/:${searchTerm}`, searchTerm);
+   public viewAllFamFriendly(famFriendly: boolean) {
+     return this.http.get<Event[]>(`${this.eventsURL}/filterAllFamFriendly/${famFriendly}`);
    }
 
-   public viewMatchingEventsByKeyword(searchTerm: String, filter: String): Observable<Event[]> {
-    if(searchTerm) { 
-      return this.http.get<Event[]>(`${this.eventsURL}/${searchTerm}/${filter}`);
-    }
-    if(!searchTerm) {
-      return this.http.get<Event[]>(`${this.eventsURL}/${filter}`)
+   public viewAllByPrice(lowPrice: number, highPrice: number) {
+     if(highPrice){
+      return this.http.get<Event[]>(`${this.eventsURL}/filterAllEntryCost/${lowPrice}/${highPrice}`)
+     } else {
+      return this.http.get<Event[]>(`${this.eventsURL}/filterAllEntryCost/${lowPrice}`)
+     }
+   }
+
+   public viewAllByFamFriendlyAndPrice(famFriendly: boolean, lowPrice: number, highPrice: number) {
+     if(highPrice){
+      return this.http.get<Event[]>(`${this.eventsURL}/filterAllFamFriendlyEntryCost/${famFriendly}/${lowPrice}/${highPrice}`)
+     } else {
+      return this.http.get<Event[]>(`${this.eventsURL}/filterAllFamFriendlyEntryCost/${famFriendly}/${lowPrice}`)
+     }
+   }
+
+   public searchByKeywordNoFilter(searchTerm: String) {
+     return this.http.get<Event[]>(`${this.eventsURL}/searchByKeyword/${searchTerm}`)
+   }
+
+   public searchByKeywordFamFriendly(searchTerm: String, famFriendly: boolean) {
+     return this.http.get<Event[]>(`${this.eventsURL}/searchByKeywordFamFriendly/${searchTerm}/${famFriendly}`);
+   }
+
+   public searchByKeywordPrice(searchTerm: String, lowPrice: number, highPrice: number) {
+    if(highPrice){ 
+      return this.http.get<Event[]>(`${this.eventsURL}/searchByKeywordPrice/${searchTerm}/${lowPrice}/${highPrice}`);
+    } else {
+      return this.http.get<Event[]>(`${this.eventsURL}/searchByKeywordPrice/${searchTerm}/${lowPrice}`);
     }
    }
+
+   public searchByKeywordFamFriendlyPrice(searchTerm: String, famFriendly: boolean, lowPrice: number, highPrice: number) {
+     if(highPrice){
+       return this.http.get<Event[]>(`${this.eventsURL}/searchByKeywordFamFriendlyPrice/${searchTerm}/${famFriendly}/${lowPrice}/${highPrice}`);
+     } else {
+       return this.http.get<Event[]>(`${this.eventsURL}/searchByKeywordFamFriendlyPrice/${searchTerm}/${famFriendly}/${lowPrice}`);
+     }
+   }
+
 }
