94a013dd9b2795548439dee429a22a0af1298d3c
Eydie <you@example.com>
2022-01-03 17:44:25 -0600

    committing recent changes trying to add Authentication Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../service/authentication.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title: string;
 
-  constructor() {
+  constructor(private loginService:AuthenticationService) {
     this.title = 'AroundTown';
+  }
+
+  ngOnInit(): {
+      
   }
 }
 
