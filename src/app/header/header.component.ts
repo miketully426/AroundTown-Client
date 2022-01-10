878c39d036fd3554f1fa0c30478432afefcd034e
Cara Noyes <cnnnoyes@outlook.com>
2022-01-10 16:17:34 -0600

    tried a listener for the hero image to disappear on scroll but it's not working yet

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  scrolled = 0;
+ 
+  constructor() { 
+    this.scrolled = this.scrolled;
+  }
 
   ngOnInit() {
+  }
+
+  @HostListener('window: scroll', ['$event'])
+  onScroll($event: Event): void {
+    let num = window.scrollY;
+    if(num >= 10) {
+      this.scrolled = 1;
+    }
+    else {
+      this.scrolled = 0;
+    }
   }
 
 }
