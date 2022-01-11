7729c69d75340aa9508f3280158d09d8448eb845
Cara Noyes <cnnnoyes@outlook.com>
2022-01-11 15:46:00 -0600

    a bunch of garbage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit{
+  showHeader: boolean = true;
+  showNav: boolean = true;
   title: string;
 
-  constructor() {
+  constructor(private router: Router) {
     this.title = 'AroundTown';
   }
+
+  ngOnInit() {
+    // listenging to routing navigation event
+    this.router.events.subscribe(event => this.modifyHeader(event));
+   }
+
+   modifyHeader(location) {
+     if(location.url != "/events" && location.url != "/addevent") {
+       this.showHeader = true;
+       this.showNav = false;
+     } else {
+      this.showHeader = false; 
+      this.showNav = true;
+     }
+   }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     FooterComponent,
     EditEventComponent,
     UserProfileComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
