68b3acfb28d526150f9ea61d3d13247db7e56529
13145 <lfallert13@yahoo.com>
2022-01-13 11:18:18 -0600

    centered slideshow, fixed dimensions on arcade image, added model binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  image1= "assets/images/AT-slideshow-party.jpg";
+  image2= "assets/images/AT-slideshow-people.jpg";
+  image3 = "assets/images/AT-slideshow-arcade.jpg";
 
+  images = [this.image1, this.image2, this.image3];
+  
   constructor() { }
 
   ngOnInit() {
