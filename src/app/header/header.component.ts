fd79a7e9f0751a0c6d3204d9d168901790bd39a7
13145 <lfallert13@yahoo.com>
2022-01-14 14:25:20 -0600

    finally have slideshow images sized, might be able to add slide with angular 9

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,17 +11,12 @@ export class HeaderComponent implements OnInit {
   image1= "assets/images/AT-slideshow-party.jpg";
   image2 = "assets/images/AT-slideshow-gate.jpg";
   image3 = "assets/images/AT-slideshow-bye.jpg";
-  
 
   images = [this.image1, this.image2, this.image3];
   
   constructor(config: NgbCarouselConfig) {  
-    config.interval = 6000;  
-    config.wrap = true;  
-    config.keyboard = false;  
-    config.pauseOnHover = false;  
-    config.showNavigationArrows = true;
-  }
+    config.interval = 6000;
+    }
 
   ngOnInit() {
   }
