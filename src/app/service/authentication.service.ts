e493f8d0829fd1e01a8cf5e75f39edf46b0d01e8
Eydie <aeydie@gmail.com>
2022-01-15 12:26:09 -0600

    moved something a line up

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -23,9 +23,8 @@ export class AuthenticationService {
     private httpClient:HttpClient
   ) { }
 
-     authenticate(username, password): Observable<any> {
-
-    return this.httpClient.post("http://localhost:8080/api/authenticate", {'username': username,'pwhash': password });
+     authenticate(username, pwhash): Observable<any> {
+      return this.httpClient.post("http://localhost:8080/api/authenticate", {'username': username,'pwhash': pwhash });
      }
      
     
