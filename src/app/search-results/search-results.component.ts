bb18aed4b0bc5a8979569a7ac4edefc703a222a3
Jessica Mertz <mertzjl91@gmail.com>
2022-01-18 22:01:33 -0600

    date search filters working. Reactive search added to date picker. Styling and display fixes added to search results and list events

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ɵɵsetComponentScope } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Event } from '../model/event'
 import { EventService } from '../service/event-service.service';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { NgbCalendar, NgbDateStruct, NgbDatepicker, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-search-results',
@@ -17,8 +18,14 @@ export class SearchResultsComponent implements OnInit {
   highPrice: number;
   faThumbsUp = faThumbsUp;
   faThumbsDown = faThumbsDown;
+  model: NgbDateStruct;
+  filterDate: {year:number, month: number, day: number};
 
-  constructor(private eventService: EventService) { }
+  @ViewChild('dp', {static: false}) dp: NgbDatepicker;
+
+  
+
+  constructor(private eventService: EventService, private calendar: NgbCalendar) { }
 
   ngOnInit() {
 
@@ -50,38 +57,66 @@ export class SearchResultsComponent implements OnInit {
       this.lowPrice = 100;
     }   
 
-  
     if(!searchTerm) {
-        if(priceFilter == "none" && familyFriendlyFilter == "none") {
+        if(priceFilter == "none" && familyFriendlyFilter == "none" && !this.filterDate) {
           this.eventService.findAll().subscribe(allEvents => 
             {this.searchedEvents = allEvents});
-        } else if (priceFilter == "none") {
+        } else if (priceFilter == "none" && !this.filterDate) {
           this.eventService.viewAllFamFriendly(this.famFriendly).subscribe(famFriendly => 
             {this.searchedEvents = famFriendly});
-        } else if(familyFriendlyFilter == "none") {
+        } else if(familyFriendlyFilter == "none" && !this.filterDate) {
             this.eventService.viewAllByPrice(this.lowPrice, this.highPrice).subscribe(allEventCost => {this.searchedEvents = allEventCost});  
-        } else {
+        } else if(!this.filterDate) {
           this.eventService.viewAllByFamFriendlyAndPrice(this.famFriendly, this.lowPrice, this.highPrice).subscribe(famFriendCost => 
             {this.searchedEvents = famFriendCost});
+        } else if(priceFilter == "none" && familyFriendlyFilter == "none" && this.filterDate) {
+          this.eventService.viewAllByDate(this.filterDate).subscribe(allDate =>
+              {this.searchedEvents = allDate});
+        } else if(this.filterDate && priceFilter == "none") {
+            this.eventService.viewAllByFamFriendlyAndDate(this.famFriendly, this.filterDate).subscribe(allFamFriendlyDate => 
+              {this.searchedEvents = allFamFriendlyDate});
+        } else if (this.filterDate && familyFriendlyFilter == "none"){
+            this.eventService.viewAllByPriceAndDate(this.filterDate, this.lowPrice, this.highPrice).subscribe(allPriceDate => 
+              {this.searchedEvents = allPriceDate});
+
+        } else {(this.eventService.viewAllByFamFriendPriceAndDate(this.filterDate, this.lowPrice, this.highPrice, this.famFriendly)).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
         }
     }
 
     if(searchTerm) {
-      if(priceFilter == "none" && familyFriendlyFilter == "none") {
+      if(priceFilter == "none" && familyFriendlyFilter == "none" && !this.filterDate) {
         this.eventService.searchByKeywordNoFilter(searchTerm).subscribe(matchingEvents => 
           {this.searchedEvents = matchingEvents});
-      } else if(priceFilter == "none") {
+      } else if(priceFilter == "none" && !this.filterDate) {
         this.eventService.searchByKeywordFamFriendly(searchTerm, this.famFriendly).subscribe(matchingEvents => 
           {this.searchedEvents = matchingEvents});
-      } else if(familyFriendlyFilter == "none") {
+      } else if(familyFriendlyFilter == "none" && !this.filterDate) {
         this.eventService.searchByKeywordPrice(searchTerm, this.lowPrice, this.highPrice).subscribe(matchingEvents => 
           {this.searchedEvents = matchingEvents});
-      } else {
+      } else if(!this.filterDate) {
         this.eventService.searchByKeywordFamFriendlyPrice(searchTerm, this.famFriendly, this.lowPrice, this.highPrice).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else if(priceFilter == "none" && familyFriendlyFilter == "none" && this.filterDate) {
+        this.eventService.searchByKeywordDate(this.filterDate, searchTerm).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else if(this.filterDate && priceFilter == "none") {
+        this.eventService.searchByKeywordDateFamFriendly(this.filterDate, searchTerm, this.famFriendly).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else if(this.filterDate && familyFriendlyFilter == "none") {
+        this.eventService.searchByKeywordDatePrice(this.filterDate, searchTerm, this.lowPrice, this.highPrice).subscribe(matchingEvents => 
+          {this.searchedEvents = matchingEvents});
+      } else {
+        this.eventService.searchByKeywordFamFriendlyPriceDate(this.filterDate, searchTerm, this.famFriendly, this.lowPrice, this.highPrice).subscribe(matchingEvents => 
           {this.searchedEvents = matchingEvents});
       }
     }  
     this.highPrice = null;
+    
 
+  }
+
+  public clearDate() {
+    this.filterDate = null;
   }
 }
